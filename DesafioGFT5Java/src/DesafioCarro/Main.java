29fc6ff426435163c4f71651e2b68689f90a20f1
DenyWild <denyswildson2011@gmail.com>
2022-06-23 18:06:38 -0300

    Carro e sua implementação concluída

--- DesafioGFT5Java/src/DesafioCarro/Main.java
+++ DesafioGFT5Java/src/DesafioCarro/Main.java
@@ -1,9 +1,42 @@
 package DesafioCarro;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+
+int quantidadeCarros = 0;
+int carrosDiesel = 0;
+int carrosGasolina = 0;
+		
+		do {
+			quantidadeCarros++;
+			
+			Carro carro = new Ferrari("lala", 5);
+			
+			System.out.println("Informe o modelo do carro: ");
+			carro.setModelo(input.nextLine());
+			
+			System.out.println("Informe a marca do carro: ");
+			carro.setMarca(input.nextLine());
+			
+			System.out.println("Informe o tipo de combustível do carro :");
+			String combustivel = input.nextLine();
+			
+			if(combustivel.equalsIgnoreCase("diesel")) {
+				carrosDiesel++;
+			}else if(combustivel.equalsIgnoreCase("gasolina")) {
+				carrosGasolina++;
+			}
+			
+			
+		}while(quantidadeCarros <= 5);
+		
+		System.out.println(" Carros movidos a diesel: "+carrosDiesel);
+		System.out.println("Carros movidos a gasolina: "+carrosGasolina);
 
 	}
 
--- DesafioGFT5Java/src/DesafioCarro/Renegade.java
+++ DesafioGFT5Java/src/DesafioCarro/Renegade.java
@@ -1,5 +1,54 @@
 package DesafioCarro;
 
-public class Renegade {
+public class Renegade extends Carro{
+	
+	private TipoCombustivel tipoCombustivel;
+	private final int VELOCIDADE_MAX = 300;
+	private int velocidadeAtual;
+	
+	
+
+	public Renegade() {
+		super();
+		
+	}
+
+	public Renegade(String modelo, String marca) {
+		super(modelo, marca);
+		
+	}
+
+	public Renegade(TipoCombustivel tipoCombustivel, int velocidadeAtual) {
+		super();
+		this.tipoCombustivel = tipoCombustivel;
+		this.velocidadeAtual = velocidadeAtual;
+	}
+
+	public void acelerar() {
+
+		if (velocidadeAtual >= 0 && velocidadeAtual <= VELOCIDADE_MAX) {
+			velocidadeAtual += 15;
+		} else {
+			velocidadeAtual = 0;
+		}
+	}
+
+	public void freiar() {
+
+		if (velocidadeAtual >= 0 && velocidadeAtual <= VELOCIDADE_MAX) {
+			velocidadeAtual -= 15;
+		} else {
+			velocidadeAtual = 0;
+		}
+
+	}
+
+	@Override
+	public String toString() {
+		return "Renegade [tipoCombustivel=" + tipoCombustivel + ", VELOCIDADE_MAX=" + VELOCIDADE_MAX
+				+ ", velocidadeAtual=" + velocidadeAtual + "]";
+	}
+	
+	
 
 }
